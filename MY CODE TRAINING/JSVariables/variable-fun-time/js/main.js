e996e3bb90e741474010c6af5726b2cc65037070
Quincy Bulla <dominionworkz@gmail.com>
2022-06-20 15:30:45 -0400

    Update to Variable Fun Time

---
+++ MY CODE TRAINING/JSVariables/variable-fun-time/js/main.js
@@ -0,0 +1,31 @@
+//--- Easy
+//create a variable and assign it a number
+let num = 47
+let number = 50
+
+//minus 10 from that number
+num = num - 10
+number = number - 20
+
+//print that number to the console
+console.log(num)
+console.log(number)
+
+//--- Medium
+//create a variable that holds a value from the input
+let inputedVal = document.querySelector('danceDanceRevolution').value
+
+let valInputed = document.querySelector
+('valueInputChallenge').value
+
+//add 25 to that number
+inputedVal = inputedVal + 25
+
+//alert that number
+alert(inputedVal)
+
+//--- Hard
+//create a variable that holds the h1
+
+
+//add an event listener to that element that console logs the sum of the two previous variables
---
+++ MY CODE TRAINING/JSVariables/variable-fun-time/js/main3.js
@@ -0,0 +1,34 @@
+//--- Easy
+//create a variable and assign it a number
+let num = 47
+let number = 50
+
+//minus 10 from that number
+num = num - 10
+number = number - 20
+
+//print that number to the console
+console.log(num)
+console.log(number)
+
+//--- Medium
+//create a variable that holds a value from the input
+let inputedVal = num(document.querySelector('#danceDanceRevolution').value)
+
+let valInputed = document.querySelector('#valueInputChallenge').value
+
+//add 25 to that number
+numinputedVal += 25
+
+valInputed = valInputed + 100
+
+//alert that number
+alert(numinputedVal)
+
+alert(valInputed)
+
+//--- Hard
+//create a variable that holds the h1
+
+
+//add an event listener to that element that console logs the sum of the two previous variables
