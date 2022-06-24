8f65f32d537d205843046bc6cf765eb84e1ba451
Quincy Bulla <dominionworkz@gmail.com>
2022-06-24 18:21:18 -0400

    updated files

---
+++ MY CODE TRAINING/JSVariables/variableExample1.js
@@ -0,0 +1,13 @@
+//A variable is a container for a value.
+
+<button id="button_A">Press Me</button>
+<h3 id="heading_A"></h3>
+
+const buttonA = document.querySelector('#button_A');
+const headingA = document.querySelector('#heading_A');
+
+buttonA.onclick = () => {
+    const name = prompt('What is your name?');
+    alert(`Hello ${name}, nice to meet you!!`);
+    headingA.textContent = `Greetings ${name}`?
+}
