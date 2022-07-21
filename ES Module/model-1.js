57ed2d848b3b2707ca81d0087538b8d76424691c
Quincy Bulla <dominionworkz@gmail.com>
2022-07-21 19:02:50 -0400

    update again

---
+++ ES Module/model-1.js
@@ -0,0 +1,10 @@
+//model-1.js
+//This "export" in front of the code makes it exportable
+//Export the "bestClubTest" variable:
+export const bestClubTest="Your Club Test"
+
+//Here is another example
+//Export the "multiply" function:
+export function multiply(x, y){
+    return x * y;
+}
---
+++ ES Module/model-2.js
@@ -0,0 +1,22 @@
+//Export Statement example this is a {...} of comma - separated names of code to be shared.
+
+// Create a variable named "bestClub":
+const bestClub = "Your Club";
+
+// Create a function named "multiply":
+function multiply(x, y) {
+  return x * y;
+}
+
+// Create an array named "fruits":
+const fruits = ["Mango", "Apple", "Orange", "Lemon"];
+
+// Export the three statements above:
+export { bestClub, multiply, fruits };
+
+//This will throw an error because the "export" keyword is inside the function
+
+function wrong(){
+    export let bestClub = "Your Club";
+    return bestClub;
+}
